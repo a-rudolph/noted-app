dc8c0b15953194d05182f9d3ab3a457ca2530074
a-rudolph <adam.rudolph@live.ca>
2022-07-27 21:57:26 -0400

    prettier print width

--- src/components/Note.tsx
+++ src/components/Note.tsx
@@ -5,7 +5,7 @@ import { FaTrash } from "react-icons/fa";
 import { useTypedSession } from "../utils/use-typed-session";
 import { trpc } from "../utils/trpc";
 
-const Note: React.FC<{ note: NoteType }> = ({ note }) => {
+const useNote = (note: NoteType) => {
   const utils = trpc.useContext();
 
   const { mutate } = trpc.useMutation("note.deleteNote", {
@@ -19,26 +19,41 @@ const Note: React.FC<{ note: NoteType }> = ({ note }) => {
 
   const isMyNote = data?.user?.id === note.authorId;
 
+  return {
+    deleteNote: () => {
+      mutate({ id: note.id });
+    },
+    isMyNote,
+  };
+};
+
+const Note: React.FC<{ note: NoteType }> = ({ note }) => {
+  const { deleteNote, isMyNote } = useNote(note);
+
   const needsBreakWord = (content: string) => {
     return !content.includes(" ");
   };
 
-  const breakWordStyle = (content: string): CSSProperties => {
-    return needsBreakWord(content) ? { wordBreak: "break-word" } : {};
+  const breakWordStyle = (
+    content: string
+  ): CSSProperties => {
+    return needsBreakWord(content)
+      ? { wordBreak: "break-word" }
+      : {};
   };
 
   return (
     <div className="prose mb-6" key={note.id}>
       <div className="flex justify-between items-baseline">
         <div className="text-2xl flex-1">
-          <span style={breakWordStyle(note.title)}>{note.title}</span>
+          <span style={breakWordStyle(note.title)}>
+            {note.title}
+          </span>
         </div>
         {isMyNote && (
           <button
             className="btn btn-link text-secondary"
-            onClick={() => {
-              mutate({ id: note.id });
-            }}
+            onClick={deleteNote}
           >
             <FaTrash />
           </button>
@@ -48,7 +63,10 @@ const Note: React.FC<{ note: NoteType }> = ({ note }) => {
         </div>
       </div>
       <hr className="m-0" />
-      <div className="py-2" style={breakWordStyle(note.content)}>
+      <div
+        className="py-2"
+        style={breakWordStyle(note.content)}
+      >
         {note.content}
       </div>
     </div>
