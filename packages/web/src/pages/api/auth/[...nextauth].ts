6fb6be496c1038034f52c91f003915e37580fcfd
a-rudolph <adam.rudolph@live.ca>
2022-08-27 15:16:55 -0400

    insist that envs are defined

--- packages/web/src/pages/api/auth/[...nextauth].ts
+++ packages/web/src/pages/api/auth/[...nextauth].ts
@@ -63,8 +63,8 @@ export default NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
     GithubProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
+      clientId: process.env.GITHUB_ID!,
+      clientSecret: process.env.GITHUB_SECRET!,
     }),
   ],
 });
